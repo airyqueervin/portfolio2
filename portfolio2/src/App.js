3e2cb5c45bc4ba19e0eb903418d0bcdd67ca7cd5
airyqueervin <airyque@gmail.com>
2018-09-30 19:32:32 -0700

    Refactored strings

--- portfolio2/src/App.js
+++ portfolio2/src/App.js
@@ -3,15 +3,16 @@ import { Container, Jumbotron } from 'reactstrap';
 import PropTypes from 'prop-types';
 import CardsList from './components/CardsList';
 import './App.css'
+import { title, headMessage, summary } from './data/strings'
 
 const App = ({ data }) => (
   <div id="top-space">
     <Container fluid>
       <Jumbotron>
-        <h1 className="display-3">Hello, I am Airyque!</h1>
-        <p className="lead">This is the hub of most of my projects.</p>
+        <h1 className="display-3">{title}</h1>
+        <p className="lead">{headMessage}</p>
         <hr className="my-2" />
-        <p>I am really excited for an opportunity that enables me to utilize my technical strengths as a software engineer and my creative strengths, gained from working as a cinematic artist, to further innovate the industry and contribute to a mission that i believe in. I'd like to continue working with cutting edge technologies, like React. I live to see my work come to life when I complete projects.</p>
+        <p>{summary}</p>
       </Jumbotron>
       <CardsList data={data} />
     </Container>
